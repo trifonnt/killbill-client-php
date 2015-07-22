fb1553f7ac95dbfa355f758f2cd1586ccd030348
trifonnt <trifont@gmail.com>
2015-07-22 15:29:09 +0300

    Fixed User creation via PHP code.

--- trifon-example-user.php
+++ trifon-example-user.php
@@ -3,8 +3,8 @@
 require_once(dirname(__FILE__) . '/lib/killbill.php');
 
 // Killbill server
-Killbill_Client::$serverUrl = "http://localhost:8080";
-Killbill_Client::$apiPassword = "test";
+Killbill_Client::$serverUrl = 'http://localhost:8080';
+Killbill_Client::$apiPassword = '';
 
 // Set these values for your particular tenant
 $tenant = new Killbill_Tenant();
@@ -16,14 +16,14 @@ var_dump( $tenant );
 
 // Prepare the User data
 $userData = new Killbill_Security();
-$userData->username = "PHP-Test-01";
-$userData->password = "PHP-Test-01";
-$userData->roles = "tenant_user";
+$userData->username = 'PHP-Test-01';
+$userData->password = 'PHP-Test-01';
+$userData->roles = array('tenant_user');
 
 var_dump( $userData );
 
 // Create it
 //                                  ($user, $reason, $comment, $headers = null
-$createdUser = $userData->createUser("Trifon", "PHP_TEST", "Test for User creation.", $tenant->getTenantHeaders());
+$createdUser = $userData->createUser('Trifon', 'PHP_TEST', 'Test for User creation.', $tenant->getTenantHeaders());
 
 var_dump( $createdUser );
