fe23927de21069006b48c9a4f491d131d5e67e45
Trifon <trifonnt@users.noreply.github.com>
2015-07-24 13:45:44 +0300

    Update trifon-example-tenant.php

--- trifon-example-tenant.php
+++ trifon-example-tenant.php
@@ -7,23 +7,24 @@ Killbill_Client::$serverUrl = 'http://localhost:8080';
 Killbill_Client::$apiPassword = '';
 
 // Set these values for your particular tenant
-$tenant = new Killbill_Tenant();
-$tenant->apiKey    = '';
-$tenant->apiSecret = '';
+//$tenant = new Killbill_Tenant();
+//$tenant->apiKey    = '';
+//$tenant->apiSecret = '';
 
-print('TRIFON');
-var_dump( $tenant );
+//print('TRIFON');
+//var_dump( $tenant );
 
 // Prepare the User data
 $tenantData = new Killbill_Tenant();
 //$tenantData->externalKey = '';
 $tenantData->apiKey = 'PHP-Test-01';
 $tenantData->apiSecret = 'PHP-Test-01';
-  
-var_dump( 'Request data = '. $tenantData );
+
+print('Request data = ');
+var_dump( $tenantData );
 
 // Create it
-// create($user, $reason, $comment)
 $createdTenant = $tenantData->create('Trifon', 'PHP_TEST', 'PHP client - Tenant creation TEST.');
 
-var_dump( 'CREATED Tenant = '. $createdTenant );
+print('CREATED Tenant = ');
+var_dump( $createdTenant );
